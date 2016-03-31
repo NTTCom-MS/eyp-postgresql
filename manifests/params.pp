d1b235acd23d31a752ff59c8d06d9533bd8bf113
Jordi Prats <jprats@systemadmin.es>
2016-03-31 12:16:07 +0200

    OS support

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,33 +1,15 @@
 class postgresql::params {
-  
+
   case $::osfamily
   {
     'redhat':
     {
       case $::operatingsystemrelease
       {
-        /^[5-7].*$/:
+        /^6.*$/:
         {
         }
         default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
-      }
-    }
-    'Debian':
-    {
-      case $::operatingsystem
-      {
-        'Ubuntu':
-        {
-          case $::operatingsystemrelease
-          {
-            /^14.*$/:
-            {
-            }
-            default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
-          }
-        }
-        'Debian': { fail('Unsupported')  }
-        default: { fail('Unsupported Debian flavour!')  }
       }
     }
     default: { fail('Unsupported OS!')  }
