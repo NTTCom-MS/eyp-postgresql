a22adcac9e3885097622fd854db8ecb8177f8306
Jordi Prats <jprats@systemadmin.es>
2016-04-04 18:44:53 +0200

    wip

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,26 @@
 #
 # === postgresql documentation
 #
-class postgresql inherits postgresql::params{
+class postgresql(
+                  $version='9.2',
+                  $datadir=$postgresql::params::datadir_default,
+                ) inherits postgresql::params {
+
+  package { $postgresql::params::reponame[$version]:
+    ensure   => 'installed',
+    source   => $postgresql::params::reposource[$version],
+    provider => $postgresql::params::repoprovider,
+  }
+
+  package { $postgresql::params::packagename:
+    ensure  => 'installed',
+    require => Package[$postgresql::params::reponame[$version]],
+  }
+
+  #initdb
+  #com a postgres
+  #-bash-4.1$ PGDATA="/var/lib/pgsql/9.2/data" /usr/pgsql-9.2/bin/initdb
+  #creates => /var/lib/pgsql/9.2/data/pg_hba.conf
 
   # service definition and notification:
   #
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,7 @@
 class postgresql::params {
 
+  #fer 9.2 minim
+
   case $::osfamily
   {
     'redhat':
@@ -8,6 +10,22 @@ class postgresql::params {
       {
         /^6.*$/:
         {
+          #TODO: es centos only
+          $datadir_default='/var/lib/pgsql/9.2/data'
+          $repoprovider = 'rpm'
+          $reposource =  {
+                          '9.2' => 'https://download.postgresql.org/pub/repos/yum/9.2/redhat/rhel-6-x86_64/pgdg-centos92-9.2-7.noarch.rpm',
+                          }
+          $reponame = {
+                        '9.2' => 'pgdg-centos92',
+                      }
+          $packagename=[ 'postgresql92', 'postgresql92-server' ]
+          $servicename = {
+                            '9.2' => 'postgresql-9.2',
+                          }
+          $inidb = {
+                      '9.2' => '/usr/pgsql-9.2/bin/initdb',
+                    }
         }
         default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
       }
