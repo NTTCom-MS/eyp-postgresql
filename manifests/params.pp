61655fdd0472f16de6c926a37f8cf68a33e15bb0
Jordi Prats <jprats@systemadmin.es>
2016-04-26 16:13:29 +0200

    suport redhat

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,37 +12,79 @@ class postgresql::params {
   {
     'redhat':
     {
-      case $::operatingsystemrelease
+      case $::operatingsystem
       {
-        /^6.*$/:
+        'RedHat':
         {
-          #TODO: es centos only
+          case $::operatingsystemrelease
+          {
+            /^6.*$/:
+            {
+              #TODO: es centos only
 
-          $version_default='9.2'
-          #TODO: modificar segons versio
-          $datadir_default='/var/lib/pgsql/9.2/data'
-          $repoprovider = 'rpm'
-          $reposource =  {
-                          '9.2' => 'http://download.postgresql.org/pub/repos/yum/9.2/redhat/rhel-6-x86_64/pgdg-centos92-9.2-7.noarch.rpm',
+              $version_default='9.2'
+              #TODO: modificar segons versio
+              $datadir_default='/var/lib/pgsql/9.2/data'
+              $repoprovider = 'rpm'
+              $reposource =  {
+                              '9.2' => 'https://download.postgresql.org/pub/repos/yum/9.2/redhat/rhel-6-x86_64/pgdg-redhat92-9.2-8.noarch.rpm',
+                              }
+              $reponame = {
+                            '9.2' => 'pgdg-redhat92',
                           }
-          $reponame = {
-                        '9.2' => 'pgdg-centos92',
-                      }
-          $packagename=[ 'postgresql92', 'postgresql92-server' ]
-          $servicename = {
-                            '9.2' => 'postgresql-9.2',
-                          }
-          $initdb = {
-                      '9.2' => '/usr/pgsql-9.2/bin/initdb',
-                    }
-          $contrib = {
-                      '9.2' => 'postgresql92-contrib',
-                    }
-          $postgresuser='postgres'
-          $postgresgroup='postgres'
-          $postgreshome='/var/lib/pgsql'
+              $packagename=[ 'postgresql92', 'postgresql92-server' ]
+              $servicename = {
+                                '9.2' => 'postgresql-9.2',
+                              }
+              $initdb = {
+                          '9.2' => '/usr/pgsql-9.2/bin/initdb',
+                        }
+              $contrib = {
+                          '9.2' => 'postgresql92-contrib',
+                        }
+              $postgresuser='postgres'
+              $postgresgroup='postgres'
+              $postgreshome='/var/lib/pgsql'
+            }
+            default: { fail("Unsupported RHEL version! - ${::operatingsystemrelease}")  }
+          }
         }
-        default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
+        'CentOS':
+        {
+          case $::operatingsystemrelease
+          {
+            /^6.*$/:
+            {
+              #TODO: es centos only
+
+              $version_default='9.2'
+              #TODO: modificar segons versio
+              $datadir_default='/var/lib/pgsql/9.2/data'
+              $repoprovider = 'rpm'
+              $reposource =  {
+                              '9.2' => 'http://download.postgresql.org/pub/repos/yum/9.2/redhat/rhel-6-x86_64/pgdg-centos92-9.2-7.noarch.rpm',
+                              }
+              $reponame = {
+                            '9.2' => 'pgdg-centos92',
+                          }
+              $packagename=[ 'postgresql92', 'postgresql92-server' ]
+              $servicename = {
+                                '9.2' => 'postgresql-9.2',
+                              }
+              $initdb = {
+                          '9.2' => '/usr/pgsql-9.2/bin/initdb',
+                        }
+              $contrib = {
+                          '9.2' => 'postgresql92-contrib',
+                        }
+              $postgresuser='postgres'
+              $postgresgroup='postgres'
+              $postgreshome='/var/lib/pgsql'
+            }
+            default: { fail("Unsupported CentOS version! - ${::operatingsystemrelease}")  }
+          }
+        }
+        default: { fail('Unsupported')}
       }
     }
     default: { fail('Unsupported OS!')  }
