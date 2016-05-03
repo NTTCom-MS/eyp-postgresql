097bc99eeef9c53aafc65472b17657c384d4c9ec
Jordi Prats <jprats@systemadmin.es>
2016-05-03 18:25:50 +0200

    dependencia rol

--- manifests/role.pp
+++ manifests/role.pp
@@ -21,28 +21,28 @@ define postgresql::role (
   postgresql_psql { "ALTER ROLE ${rolename} ENCRYPTED PASSWORD":
     command => "ALTER ROLE \"${rolename}\" ${password_sql}",
     unless  => "SELECT usename FROM pg_shadow WHERE usename='${rolename}' and passwd='${password_hash_sql}'",
-    require => Class['::postgresql::service'],
+    require => [Class['::postgresql::service'], Postgresql_psql["CREATE ROLE ${rolename}"]],
   }
 
   $login_sql=$login ? { true => 'LOGIN', default => 'NOLOGIN' }
 
   postgresql_psql {"ALTER ROLE \"${rolename}\" ${login_sql}":
     unless  => "SELECT rolname FROM pg_roles WHERE rolname ='${rolename}' and rolcanlogin =${login}",
-    require => Class['::postgresql::service'],
+    require => [Class['::postgresql::service'], Postgresql_psql["CREATE ROLE ${rolename}"]],
   }
 
   $superuser_sql=$superuser ? { true => 'SUPERUSER', default => 'NOSUPERUSER' }
 
   postgresql_psql {"ALTER ROLE \"${rolename}\" ${superuser_sql}":
     unless  => "SELECT rolname FROM pg_roles WHERE rolname ='${rolename}' and rolsuper =${superuser}",
-    require => Class['::postgresql::service'],
+    require => [Class['::postgresql::service'], Postgresql_psql["CREATE ROLE ${rolename}"]],
   }
 
   $replication_sql=$replication ? { true => 'REPLICATION', default => '' }
 
   postgresql_psql {"ALTER ROLE \"${rolename}\" ${replication_sql}":
     unless  => "SELECT rolname FROM pg_roles WHERE rolname ='${rolename}' and rolreplication =${replication}",
-    require => Class['::postgresql::service'],
+    require => [Class['::postgresql::service'], Postgresql_psql["CREATE ROLE ${rolename}"]],
   }
 
   #
