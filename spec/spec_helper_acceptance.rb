2a222a4e47ee8fb4ffade97bb6f8ce8e4ae1f6b6
Jordi Prats <jordi.prats@gmail.com>
2018-10-04 17:39:04 +0200

    fix accepntace

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,11 +1,10 @@
 require 'beaker-rspec'
-require 'beaker/puppet_install_helper'
 
-run_puppet_install_helper
+install_puppet_agent_on hosts, {}
 
 RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  module_name = module_root.split('-').last
 
   # Readable test descriptions
   c.formatter = :documentation
@@ -13,9 +12,8 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'postgresql')
+    puppet_module_install(:source => module_root, :module_name => module_name)
     hosts.each do |host|
-      # dependencies
       on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module', 'install', 'puppetlabs-concat'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module', 'install', 'eyp-eyplib'), { :acceptable_exit_codes => [0,1] }
