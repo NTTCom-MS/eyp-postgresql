36ac2078a498ef5b986eb9a8f710ef14000476b5
Jordi Prats <jordi.prats@gmail.com>
2018-11-26 12:56:53 +0100

    skel

--- files/backup_snapshot.py
+++ files/backup_snapshot.py
@@ -1,6 +1,42 @@
 import getopt
 import sys
 import subprocess
+import os.path
+
+# thank god for stackoverflow - https://stackoverflow.com/questions/25283882/determining-the-filesystem-type-from-a-path-in-python
+def GetFSType(path):
+    partition = {}
+    for part in psutil.disk_partitions(True):
+        partition[part.mountpoint] = (part.fstype, part.device)
+    if path in partition:
+        return partition[path]
+    splitpath = path.split(os.sep)
+    for i in xrange(len(splitpath),0,-1):
+        path = os.sep.join(splitpath[:i]) + os.sep
+        if path in partition:
+            return partition[path]
+        path = os.sep.join(splitpath[:i])
+        if path in partition:
+            return partition[path]
+    return ("unkown","none")
+
+def getDataDir():
+    #psql -U postgres -c 'SHOW data_directory;'
+    p = subprocess.Popen("psql -U postgres -c 'SHOW data_directory;' | grep -A 1 -- --- | tail -n1", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    linecount=0
+    lastline=""
+    for line in p.stdout.readlines():
+        lastline = line.strip()
+        linecount+=1
+    retval = p.wait()
+
+    if retval==0 and linecount==1:
+        print lastline
+        return lastline
+    else:
+        logging.error('Error getting datadir')
+        sys.exit('Error getting datadir')
+
 
 def getVG(lvm_disk):
     # busquem vg del lv
@@ -25,27 +61,37 @@ def getVG(lvm_disk):
                 if line_split[0]=="PV" and line_split[1]=="Name":
                     pv_disks.append(line_split[2])
                 else:
+                    logging.error('Corrupted output getting PV disk for '+vg_name)
                     sys.exit('Corrupted output getting PV disk for '+vg_name+':\n'+line)
             retval = p.wait()
 
             if retval!=0:
+                logging.error('ERROR listing PV disks for: '+vg_name)
                 sys.exit('ERROR listing PV disks for: '+vg_name)
             else:
                 print pv_disks
 
 
         else:
+            logging.error('Corrupted output getting VG name: '+lastline)
             sys.exit('Corrupted output getting VG name: '+lastline)
     else:
+        logging.error('Invalid disk: '+lvm_disk)
         sys.exit('Invalid disk: '+lvm_disk)
 
 lvm_disk = ""
 snapshot_size = "10G"
 awscli = False
+config_file = './postgres_snapshot.config'
+logdir = '.'
 
-options, remainder = getopt.getopt(sys.argv[1:], 'l:s:a', ['lvm-disk=',
-                                                          'snapshot-size=',
-                                                          'aws'
+# parse opts
+
+options, remainder = getopt.getopt(sys.argv[1:], 'l:s:ac:', [
+                                                            'lvm-disk=',
+                                                            "config="
+                                                            'snapshot-size=',
+                                                            'aws'
                                                          ])
 
 for opt, arg in options:
@@ -53,13 +99,55 @@ for opt, arg in options:
         lvm_disk = arg
     elif opt in ('-s', '--snapshot-size'):
         snapshot_size = arg
+    elif opt in ('-c', '--config'):
+        config_file = arg
     elif opt in ('-a', '--aws'):
         awscli = True
+    elif opt in ('-l', '--logdir'):
+        logdir = arg
     else:
       sys.exit("unrecoginzed option: ".opt)
 
-if not lvm_disk:
-    sys.exit("lvm-disk is mandatory")
+if not os.path.isfile(config_file):
+    logging.error("Error - config file NOT FOUND ("+config_file+")")
+    sys.exit(1)
+
+if not access(config_file, R_OK):
+    logging.error("Error reading config file ("+config_file+")")
+    sys.exit(1)
+
+try:
+    config = SafeConfigParser()
+    config.read(config_file)
+except Exception, e:
+    logging.error("error reading config file - ABORTING - "+str(e))
+    sys.exit(1)
+
+try:
+    logdir=config.get('rsyncman', 'logdir').strip('"')
+except:
+    logdir=os.path.dirname(os.path.abspath(config_file))
+
+ts = time.time()
+
+logFile = "{0}/{1}/{2}-{3}.log".format(logdir, datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d'), 'rsyncman', datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d-%H%M%S'))
+
+current_day_dirname = os.path.dirname(logFile)
+
+try:
+    os.makedirs(current_day_dirname)
+except Exception, e:
+    logging.debug("WARNING - error creating log directory: "+current_day_dirname+" - "+str(e))
+
+fileHandler = logging.FileHandler(logFile)
+fileHandler.setFormatter(logFormatter)
+rootLogger.addHandler(fileHandler)
+
+rootLogger.setLevel(0)
+
+GetFSType(getDataDir())
+
+sys.exit("FI")
 
 getVG(lvm_disk)
 
---
+++ manifests/backup/snapshot.pp
@@ -0,0 +1,76 @@
+define postgresql::backup::snapshot (
+                                      $destination,
+                                      $backupname       = $name,
+                                      $pgroot           = undef,
+                                      $instance         = undef,
+                                      $retention        = '7',
+                                      $dbs              = 'ALL',
+                                      $mailto           = undef,
+                                      $idhost           = undef,
+                                      $basedir          = '/usr/local/bin',
+                                      $ensure           = 'present',
+                                      $username         = 'postgres',
+                                      #cron
+                                      $setcronjob       = true,
+                                      $hour_cronjob     = '2',
+                                      $minute_cronjob   = '0',
+                                      $month_cronjob    = undef,
+                                      $monthday_cronjob = undef,
+                                      $weekday_cronjob  = undef,
+                                    ) {
+
+  Exec {
+    path => '/usr/sbin:/usr/bin:/sbin:/bin',
+  }
+
+  validate_absolute_path($basedir)
+  validate_absolute_path($destination)
+
+  #source => "puppet:///modules/${module_name}/backup_pgdump.sh",
+  file { "${basedir}/postgres_snapshot.py":
+    ensure  => $ensure,
+    owner   => 'root',
+    group   => $username,
+    mode    => '0750',
+    content => file("${module_name}/backup_snapshot.py"),
+  }
+
+  file { "${basedir}/postgres_snapshot.config":
+    ensure  => $ensure,
+    owner   => 'root',
+    group   => $username,
+    mode    => '0640',
+    content => template("${module_name}/backup/backup_pgdump_config.erb"),
+  }
+
+  exec { "mkdir p ${destination} backup":
+    command => "mkdir -p ${destination}",
+    creates => $destination,
+  }
+
+  file { $destination:
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => $username,
+    mode    => '0770',
+    require => Exec["mkdir p ${destination} backup"],
+  }
+
+  if($setcronjob)
+  {
+    cron { "cronjob logical postgres backup: ${backupname}":
+      ensure   => $ensure,
+      command  => "${basedir}/pgdumbackup_snapshotpbackup.py",
+      user     => $username,
+      hour     => $hour_cronjob,
+      minute   => $minute_cronjob,
+      month    => $month_cronjob,
+      monthday => $monthday_cronjob,
+      weekday  => $weekday_cronjob,
+      require  => File[ [ "${basedir}/postgres_snapshot.config",
+                          "${basedir}/postgres_snapshot.py"
+                      ] ],
+    }
+  }
+
+}
