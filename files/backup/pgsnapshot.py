611d72fc7c32977f8fcbbdb3e883766673d10f69
Jordi Prats <jordi.prats@gmail.com>
2018-11-28 18:02:40 +0100

    prova

--- files/backup/pgsnapshot.py
+++ files/backup/pgsnapshot.py
@@ -294,7 +294,15 @@ def createAWSsnapshot(ec2, volume_id, lvm_disk, snap_name):
         # result = response[volume_id]
         # ec2.create_tags(Resources=[result],Tags=[{ 'Key': 'pgsnapshot-lvm_disk', 'Value': lvm_disk },{ 'Key': 'pgsnapshot-host', 'Value': id_host },{ 'Key': 'pgsnapshot-snap_name', 'Value': snap_name }])
         snapshot = ec2.create_snapshot(VolumeId=volume_id, Description="pgsnapshot for "+snap_name)
-        snapshot.add_tags({ 'Key': 'pgsnapshot-lvm_disk', 'Value': lvm_disk },{ 'Key': 'pgsnapshot-host', 'Value': id_host },{ 'Key': 'pgsnapshot-snap_name', 'Value': snap_name })
+        #snapshot.add_tags({ 'Key': 'pgsnapshot-lvm_disk', 'Value': lvm_disk },{ 'Key': 'pgsnapshot-host', 'Value': id_host },{ 'Key': 'pgsnapshot-snap_name', 'Value': snap_name })
+        ec2.create_tags(
+            Resources=[
+                snapshot['SnapshotId'],
+            ],
+            Tags=[
+                { 'Key': 'pgsnapshot-lvm_disk', 'Value': lvm_disk },{ 'Key': 'pgsnapshot-host', 'Value': id_host },{ 'Key': 'pgsnapshot-snap_name', 'Value': snap_name }
+            ]
+ )
 
         return True
     except Exception as e:
