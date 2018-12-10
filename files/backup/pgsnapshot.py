695a2936f91d49040a384c1865058ea8196b5674
Jordi Prats <jordi.prats@gmail.com>
2018-12-10 17:41:20 +0100

    count

--- files/backup/pgsnapshot.py
+++ files/backup/pgsnapshot.py
@@ -662,6 +662,16 @@ def launchAWSInstanceBasedOnInstanceIDwithSnapshots(base_instance_id, snap_name,
         reservations = searchForRestoredInstance(id_host, lvm_disk, snap_name)
         logging.debug("reservations: "+str(reservations))
 
+        running_restores=0
+        for reservation in reservations:
+            logging.debug("reservation: "+str(reservation))
+            for instance in reservation['Instances']:
+                logging.debug(instance['InstanceId']+": "+instance['State']['Name'])
+                if instance['State']['Name']!='terminated':
+                    running_restores+=1
+
+        # assert: running restores ha de ser 1
+
         #
         # Linux Devices: /dev/sdf through /dev/sdp
         #
