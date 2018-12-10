0f52fcade0cda661ce5bcec4e222951ca79504f2
Jordi Prats <jordi.prats@gmail.com>
2018-12-10 17:36:34 +0100

    typo

--- files/backup/pgsnapshot.py
+++ files/backup/pgsnapshot.py
@@ -616,7 +616,7 @@ def launchAWSInstanceBasedOnInstanceIDwithSnapshots(base_instance_id, snap_name,
 
     running_restores=0
     for reservation in reservations:
-        for instance in reservation:
+        for instance in reservation['Instances']:
             logging.debug(instance['InstanceId']+": "+instance['State']['Name'])
             if instance['State']['Name']!='terminated':
                 running_restores+=1
