653ed1a743f94f17e95faa31fae3eaf87e14d01e
Jordi Prats <jordi.prats@gmail.com>
2018-12-12 12:44:12 +0100

    snaprestore

--- files/pgsnapshot/pgsnapshot.py
+++ files/pgsnapshot/pgsnapshot.py
@@ -702,8 +702,7 @@ def launchAWSInstanceBasedOnInstanceIDwithSnapshots(base_instance_id, snap_name,
                                 curl https://raw.githubusercontent.com/jordiprats/puppet-masterless/master/setup.sh | bash
                                 /opt/puppet-masterless/localpuppetmaster.sh -d /tmp/lvm -r https://github.com/NTTCom-MS/eyp-lvm -s /tmp/lvm/modules/lvm/examples/base.pp
                                 /opt/puppet-masterless/localpuppetmaster.sh -d /tmp/postgres -r https://github.com/jordiprats/eyp-postgresql -s /tmp/postgres/modules/postgresql/examples/pgsnaprestore.pp
-                                /usr/local/bin/pgsnaprestore.sh
-                                """,
+                                /usr/local/bin/pgsnaprestore.sh """+snap_name,
                                 TagSpecifications=[
                                                     {
                                                         'ResourceType': 'instance',
