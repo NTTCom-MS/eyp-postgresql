fdb897d94e318dd7ebfc5112cf13be554980c6e7
Jordi Prats <jordi.prats@gmail.com>
2018-12-12 17:37:27 +0100

    typo

--- files/pgsnapshot/pgsnapshot.py
+++ files/pgsnapshot/pgsnapshot.py
@@ -722,7 +722,7 @@ def launchAWSInstanceBasedOnInstanceIDwithSnapshots(base_instance_id, snap_name,
                                 curl https://raw.githubusercontent.com/jordiprats/puppet-masterless/master/setup.sh | bash
                                 /opt/puppet-masterless/localpuppetmaster.sh -d /tmp/lvm -r https://github.com/NTTCom-MS/eyp-lvm -s /tmp/lvm/modules/lvm/examples/base.pp
                                 /opt/puppet-masterless/localpuppetmaster.sh -d /tmp/postgres -r https://github.com/jordiprats/eyp-postgresql -s /tmp/postgres/modules/postgresql/examples/pgsnaprestore.pp
-                                /usr/local/bin/pgsnaprestore.sh """+snap_name,
+                                /usr/local/bin/pgsnaprestore.sh """+snap_name+"\n",
                                 TagSpecifications=[
                                                     {
                                                         'ResourceType': 'instance',
