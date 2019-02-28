76b9dd8ef800f5a9296b3a75fbf006bb94a08f78
Jordi Prats <jordi.prats@gmail.com>
2019-02-28 17:43:03 +0100

    lint

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,22 +1,16 @@
 class postgresql::service inherits postgresql {
-  #
-  validate_bool($manage_docker_service)
-  validate_bool($manage_service)
-  validate_bool($enable)
-
-  validate_re($ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${ensure}")
 
   $is_docker_container_var=getvar('::eyp_docker_iscontainer')
   $is_docker_container=str2bool($is_docker_container_var)
 
   if( $is_docker_container==false or
-      $manage_docker_service)
+      $postgresql::manage_docker_service)
   {
-    if($manage_service)
+    if($postgresql::manage_service)
     {
-      service { $postgresql::params::servicename[$version]:
-        ensure => $ensure,
-        enable => $enable,
+      service { $postgresql::params::servicename[$postgresql::version]:
+        ensure => $postgresql::ensure,
+        enable => $postgresql::enable,
       }
     }
   }
