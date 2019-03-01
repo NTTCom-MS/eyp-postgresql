dcde172c945184713c6d7527b8c3072e4ab99b56
Jordi Prats <jordi.prats@gmail.com>
2019-03-01 13:57:41 +0100

    demo 9.2

---
+++ examples/demo_pg92.pp
@@ -0,0 +1,22 @@
+class { 'postgresql':
+  wal_level           => 'hot_standby',
+  max_wal_senders     => '3',
+  checkpoint_segments => '8',
+  wal_keep_segments   => '8',
+  port                => '5432'
+}
+
+postgresql::hba_rule { 'test':
+  user     => 'replicator',
+  database => 'replication',
+  address  => '192.168.56.0/24',
+}
+
+postgresql::role { 'replicator':
+  replication => true,
+  password    => 'replicatorpassword',
+}
+
+postgresql::schema { 'jordi':
+  owner => 'replicator',
+}
