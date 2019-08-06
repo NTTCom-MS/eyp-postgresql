b0ee751ac7e2e0128099f25c33e625f0dc78eef0
Jordi Prats <jordi.prats@gmail.com>
2019-08-06 10:13:21 +0200

    change port no restart

---
+++ examples/demo_pg10_change_port_no_restart.pp
@@ -0,0 +1,23 @@
+class { 'postgresql':
+  wal_level           => 'hot_standby',
+  max_wal_senders     => '3',
+  checkpoint_segments => '8',
+  wal_keep_segments   => '8',
+  version             => '10',
+  port                => '6432',
+  restart_if_needed   => false,
+}
+
+postgresql::pgdumpbackup { 'demobackup':
+  destination => '/tmp',
+}
+
+postgresql::hba_rule { 'demo':
+  user     => 'demo',
+  database => 'demo',
+  address  => '192.168.0.0/16',
+}
+
+postgresql::role { 'demo':
+  password    => 'demopass',
+}
