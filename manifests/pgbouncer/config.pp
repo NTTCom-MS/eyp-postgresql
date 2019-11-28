81a86ab0e841227d134247466c4c71d80cbef291
Jordi Prats <jordi.prats@gmail.com>
2019-11-28 17:33:41 +0100

    varis

--- manifests/pgbouncer/config.pp
+++ manifests/pgbouncer/config.pp
@@ -13,4 +13,17 @@ class postgresql::pgbouncer::config inherits postgresql::pgbouncer {
     content => template("${module_name}/pgbouncer/pgbouncer.erb"),
   }
 
+  concat { '/etc/pgbouncer/pgbouncer-databases.ini':
+    ensure => 'present',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  concat::fragment{ 'base pgbouncer databases':
+    order   => '00',
+    target  => '/etc/pgbouncer/pgbouncer-databases.ini',
+    content => template("${module_name}/pgbouncer/databases-header.erb"),
+  }
+
 }
