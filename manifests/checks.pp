07b0e29229986072152d04fe05029e81ba65cf33
Jordi Prats <jordi.prats@gmail.com>
2020-02-03 16:33:56 +0100

    add sudos nrpe

--- manifests/checks.pp
+++ manifests/checks.pp
@@ -1,6 +1,7 @@
 class postgresql::checks(
-                          $ensure  = 'present',
-                          $basedir = '/usr/local/bin',
+                          $ensure         = 'present',
+                          $basedir        = '/usr/local/bin',
+                          $add_nrpe_sudos = true,
                         ) inherits postgresql::params {
   exec { "mkdir p ${basedir}":
     command => "mkdir -p ${basedir}",
@@ -15,5 +16,12 @@ class postgresql::checks(
     mode    => '0755',
     content => file("${module_name}/nagios/check_replication_lag.sh"),
     require => Exec["mkdir p ${basedir}"],
+  }
+
+  if($add_nrpe_sudos)
+  {
+    nrpe::sudo { 'sudo NRPE check_replication_lag':
+      command => "${basedir}/check_replication_lag",
+    }
   }
 }
