65e8fb1b0bf123b8ae8d3671c962aa83ccc102ba
Jordi Prats <jordi.prats@gmail.com>
2020-03-19 16:31:05 +0100

    check datadir compliance

--- manifests/checks.pp
+++ manifests/checks.pp
@@ -18,10 +18,23 @@ class postgresql::checks(
     require => Exec["mkdir p ${basedir}"],
   }
 
+  file { "${basedir}/check_postgres_datadir":
+    ensure  => $ensure,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => file("${module_name}/nagios/check_postgres_datadir.sh"),
+    require => Exec["mkdir p ${basedir}"],
+  }
+
   if($add_nrpe_sudos)
   {
     nrpe::sudo { 'sudo NRPE check_replication_lag':
       command => "${basedir}/check_replication_lag",
+    }
+
+    nrpe::sudo { 'sudo NRPE check_postgres_datadir':
+      command => "${basedir}/check_postgres_datadir",
     }
   }
 }
