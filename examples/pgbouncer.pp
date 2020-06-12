f4b0772bb85aee06e80675efb91d579e60d4ad3b
Jordi Prats <jordi.prats@gmail.com>
2020-06-12 16:31:36 +0200

    exemple pgbouncer

---
+++ examples/pgbouncer.pp
@@ -0,0 +1,9 @@
+class { 'postgresql':
+  wal_level           => 'hot_standby',
+  max_wal_senders     => '3',
+  checkpoint_segments => '8',
+  wal_keep_segments   => '8',
+  version             => '11',
+}
+
+class { 'pgbouncer': }
